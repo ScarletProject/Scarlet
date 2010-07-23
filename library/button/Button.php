24f1c3ad9c0bf4b8cc7bd9dd1fc2e913d93eb006
= <mattmuelle@gmail.com>
2010-07-23 16:03:36 -0500

    Removed the random color generator for button

--- library/button/Button.php
+++ library/button/Button.php
@@ -15,14 +15,15 @@ class Button extends Tag
 		$this->defaults('value', 'click');
 		
 		// Generate a random color
-		mt_srand((double)microtime()*1000000);
-	    $c = '';
-	    while(strlen($c)<6){
-	        $c .= sprintf("%02X", mt_rand(0, 255));
-	    }		
+		// mt_srand((double)microtime()*1000000);
+		// 	    $c = '';
+		// 	    while(strlen($c)<6){
+		// 	        $c .= sprintf("%02X", mt_rand(0, 255));
+		// 	    }		
 		
-		$this->give('button.css', 'color', '#'.$c);
+		// $this->give('button.css', 'color', '#'.$c);
 		$this->script('jquery');
+		$this->stylesheet('button.css');
 		$this->give('button.js', array('click' => $this->arg('click'), 'hover' => $this->arg('hover')));
 		$this->give('css:rounded.css', 'roundness', '10px');
 		
