469573d17a7ab3aa5bd95f1339300f9fa3031a07
= <mattmuelle@gmail.com>
2010-10-16 02:03:28 -0500

    Added 'naked' attribute to display chromeless browser, also removed id attribute for first release.

--- library/video/youtube/chromeless/chromeless.php
+++ library/video/youtube/chromeless/chromeless.php
@@ -13,24 +13,31 @@
 class video_youtube_chromeless extends tag
 {
 	
-	function init() {
-		$this->defaults('query = Macaw birds in the Amazon jungle');
-		$query = urlencode($this->arg('query'));
-		$json = file_get_contents('http://gdata.youtube.com/feeds/api/videos?q='.$query.'&max-results=1&v=2&alt=jsonc');
+	function setup() {
+		$this->defaults('video');
 		
-		$video = json_decode($json);
-		$video = current($video->data->items);
+		// If an video id is given, just use that.
+		if($this->arg('id')) {
+			$this->video_id = $this->arg('id');
+			return;
+		} elseif($this->arg('video')) {
+			$query = urlencode($this->arg('video'));
+			$json = file_get_contents('http://gdata.youtube.com/feeds/api/videos?q='.$query.'&max-results=1&v=2&alt=jsonc');
+		
+			$video = json_decode($json);
+			$video = current($video->data->items);
 
-		$this->video_id = $video->id;
+			$this->video_id = $video->id;
+		}
 		
 		$this->give('chromeless.js', 'videoid', $this->video_id);
 
-		$this->width(480);
-		$this->height(295);
+		$this->width(640);
+		$this->height(385);
 	}
 	
-	function tostring() {
-		 return '<object width="480" height="295" type="application/x-shockwave-flash" id="ytPlayer" data="http://www.youtube.com/apiplayer?&amp;enablejsapi=1&amp;playerapiid=player1&amp;id"><param name="allowScriptAccess" value="always"></object>';
+	function show() {
+		 return '<object width="640" height="385" type="application/x-shockwave-flash" id="ytPlayer" data="http://www.youtube.com/apiplayer?&amp;enablejsapi=1&amp;playerapiid=player1&amp;id"><param name="allowScriptAccess" value="always"></object>';
 		
 	}
 }
--- library/video/youtube/youtube.php
+++ library/video/youtube/youtube.php
@@ -12,14 +12,14 @@ class video_youtube extends tag
 {
 	private $video_id;
 	
-	function init() {
+	function setup() {
 		$this->defaults('video');
 		
-		// If an video id is given, just use that.
-		if($this->arg('id')) {
-			$this->video_id = $this->arg('id');
-			return;
-		} elseif($this->arg('video')) {
+		// // If an video id is given, just use that.
+		// if($this->arg('id')) {
+		// 	$this->video_id = $this->arg('id');
+		// 	return;
+		// } elseif($this->arg('video')) {
 			$query = urlencode($this->arg('video'));
 			$json = file_get_contents('http://gdata.youtube.com/feeds/api/videos?q='.$query.'&max-results=1&v=2&alt=jsonc');
 		
@@ -27,11 +27,18 @@ class video_youtube extends tag
 			$video = current($video->data->items);
 
 			$this->video_id = $video->id;
-		}		
+		// }		
 	}
 	
-	function tostring() {
+	function show() {
 		if(!isset($this->video_id)) return '';
+		
+		// Run the chromeless browser
+		if($this->arg('naked')) {
+			// Don't need to wrap twice - other tag will wrap.
+			$this->wrap(false);
+			return S('video:youtube:chromeless')->arg('video', $this->arg('video'));
+		}
 		
 		 return '<object width="640" height="385"><param name="movie" value="http://www.youtube.com/v/'.$this->video_id.'?fs=1&amp;hl=en_US"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/'.$this->video_id.'?fs=1&amp;hl=en_US" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="640" height="385"></embed></object>';
 		
